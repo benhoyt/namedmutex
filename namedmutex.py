6ce6a87566e47fc8c89553c4db0ed7f30564bda5
Ben Hoyt <benhoyt@gmail.com>
2022-08-25 10:05:15 +1200

    Switch to using CreateMutexW explicitly

--- namedmutex.py
+++ namedmutex.py
@@ -12,7 +12,7 @@ import ctypes
 from ctypes import wintypes
 
 # Create ctypes wrapper for Win32 functions we need, with correct argument/return types
-_CreateMutex = ctypes.windll.kernel32.CreateMutexA
+_CreateMutex = ctypes.windll.kernel32.CreateMutexW
 _CreateMutex.argtypes = [wintypes.LPCVOID, wintypes.BOOL, wintypes.LPCWSTR]
 _CreateMutex.restype = wintypes.HANDLE
 
