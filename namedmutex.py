daa7d36ffbe844a893d6ae71d61f7030ec05c9fa
Ben Hoyt <benhoyt@gmail.com>
2022-08-25 09:52:09 +1200

    Change to CreateMutexW

--- namedmutex.py
+++ namedmutex.py
@@ -12,8 +12,8 @@ import ctypes
 from ctypes import wintypes
 
 # Create ctypes wrapper for Win32 functions we need, with correct argument/return types
-_CreateMutex = ctypes.windll.kernel32.CreateMutexA
-_CreateMutex.argtypes = [wintypes.LPCVOID, wintypes.BOOL, wintypes.LPCSTR]
+_CreateMutex = ctypes.windll.kernel32.CreateMutexW
+_CreateMutex.argtypes = [wintypes.LPCVOID, wintypes.BOOL, wintypes.LPCWSTR]
 _CreateMutex.restype = wintypes.HANDLE
 
 _WaitForSingleObject = ctypes.windll.kernel32.WaitForSingleObject
@@ -108,8 +108,5 @@ class NamedMutex(object):
 
 if __name__ == '__main__':
     # Just test that acquire and release work.
-    print(ctypes.windll.kernel32.CreateMutexA)
-    print(ctypes.windll.kernel32.CreateMutexW)
-    print(ctypes.windll.kernel32.CreateMutexA == ctypes.windll.kernel32.CreateMutexW)
     with NamedMutex('test_mutex_123'):
         pass
